5143191b25688498af4888b4d9e7520b38ef70fc
VOL1TZ1 <91470979+VOL1TZ1@users.noreply.github.com>
2024-05-10 10:58:20 +0300

    Navigate to Banner TestCase

---
+++ src/test/java/Navigate/NavigateToBanner.java
@@ -0,0 +1,35 @@
+package Navigate;
+
+import com.shaft.driver.SHAFT;
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class NavigateToBanner {
+    SHAFT.GUI.WebDriver driver;
+    String siteURL = "https://demo.nopcommerce.com/";
+    String siteTitle = "nopCommerce demo store";
+    String expectedSite ="https://demo.nopcommerce.com/cell-phones?viewmode=grid&orderby=0&pagesize=6";
+    By bannerImage= By.className("nivo-main-image");
+    @BeforeMethod
+    public void runThisFirst(){
+        // Create new driver object
+        driver = new SHAFT.GUI.WebDriver();
+        // Go to the website
+        driver.browser().navigateToURL(siteURL);
+        // To ensure that the site loaded and there is no problem in the connection
+        driver.verifyThat().browser().title().isEqualTo(siteTitle).perform();
+    }
+    @Test
+    public void Banner(){
+        driver.element().click(bannerImage);
+        //String actualSite =driver.browser().getCurrentURL();
+        driver.verifyThat().browser().title().isEqualTo(expectedSite).perform();
+    }
+    @AfterMethod
+    public void runThisLast(){
+        driver.quit();
+        SHAFT.Properties.reporting.openAllureReportAfterExecution();
+    }
+}
